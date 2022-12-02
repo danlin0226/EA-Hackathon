7a0437b78bb709e0f717d05b1daab8aa412b64bd
Caty Heng <105816942+Caty115@users.noreply.github.com>
2022-12-02 11:36:42 -0800

    requires styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.scss";
 import Home from "./components/home/Home";
 import ToolTipOne from "./components/tool-tip-one/ToolTipOne";
+import ToolTipTwo from "./components/tool-tip-two/ToolTipTwo";
+import ToolTipThree from "./components/tool-tip-three/ToolTipThree";
 import "./main.scss";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -11,7 +13,11 @@ function App() {
       <Home />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/1" element={<Home />} />
           <Route path="/2" element={<ToolTipOne />} />
+          <Route path="/3" element={<ToolTipTwo />} />
+          <Route path="/4" element={<ToolTipThree />} />
         </Routes>
       </BrowserRouter>
     </>
