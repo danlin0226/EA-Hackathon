5c26c9078b9d33d627a4c524966fe28dcefa30c0
Caty Heng <105816942+Caty115@users.noreply.github.com>
2022-12-02 12:31:31 -0800

    resolve conflict in Home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import screenshot from "../../assets/EA.png";
 import "./Home.scss";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import ClickAwayListener from "@mui/material/ClickAwayListener";
-import { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
-import { styled } from "@mui/material/styles";
-import closeButton from "../../assets/exit.png";
-import { Link } from "react-router-dom";
+
 
 
 
